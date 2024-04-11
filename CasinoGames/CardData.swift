a4dd1a3e3635cd1029af6509ec5c9ee9f5b00d1a
goromajima <goromajimaisreal12834@gmail.com>
2024-04-11 11:49:27 -0400

    cards fully done babyyyyy

---
+++ CasinoGames/CardData.swift
@@ -0,0 +1,25 @@
+//
+//  CardData.swift
+//  CasinoGames
+//
+//  Created by Christopher Leonard on 4/11/24.
+//
+
+import Foundation
+
+struct Card: Hashable {
+    var suit: String
+    var symbol: String
+    var imageName:String = ""
+    var value: Int
+    var id:String = UUID().uuidString
+    
+    init(suit: String, symbol: String, value: Int) {
+        self.suit = suit
+        self.symbol = symbol
+        self.value = value
+        
+        imageName = "card\(suit)\(symbol)"
+    }
+    
+}
