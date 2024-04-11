183b817a6824b8d3a992b3f28e5b5917310c4459
Michael Potter <mpotter0905@gmail.com>
2024-04-11 11:08:14 -0400

    Initial Commit

---
+++ CasinoGames/CasinoGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasinoGamesApp.swift
+//  CasinoGames
+//
+//  Created by Michael Potter on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CasinoGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
